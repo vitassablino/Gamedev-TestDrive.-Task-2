871f76ed60446476321b0883785bef1f7422a3b1
vitassablino <105976325+vitassablino@users.noreply.github.com>
2022-05-22 11:34:33 +0300

    Добавьте файлы проекта.

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        byte AZNV;
+        sbyte flyDetection;
+        short flyRoll;
+        ushort fuelLevel;
+        int pointDistance;
+        uint flyNumber;
+        long flySpeed;
+        ulong dlyHeight;
+        float signalGain;
+        double flyAmount;
+    }
+}
