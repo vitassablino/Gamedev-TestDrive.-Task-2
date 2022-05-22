b71ccf2b81b838a32d4231c8ed4de2a834dc94a7
vitassablino <105976325+vitassablino@users.noreply.github.com>
2022-05-22 11:34:33 +0300

    Добавьте файлы проекта.

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        byte b;
+        sbyte sb;
+        short s;
+        ushort us;
+        int i;
+        uint ui;
+        long l;
+        ulong ul;
+        float f;
+        double d;
+    }
+}
